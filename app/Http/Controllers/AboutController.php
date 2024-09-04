946db57af21a06c18bb71f3e211d04063b0a3cfb
kimutaiRop <kimrop19@gmail.com>
2024-09-04 09:26:09 +0300

    add rank

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use App\Models\Sponsor;
+use App\Models\TeamMember;
+use App\Models\Professional;
 
 use Illuminate\Http\Request;
 
@@ -12,8 +14,9 @@ class AboutController extends Controller
     public function about()
     {
         $events = Event::whereDate('date_time', '>=', date('Y-m-d'))->orderBy('date_time', 'asc')->take(8)->get();
-        $sponsors = Sponsor::all();
-        $coming_event = Event::whereDate('date_time', '>=', date('Y-m-d'))->orderBy('date_time', 'asc')->first();
-        return view('home', compact("events", "coming_event", "sponsors"));
+        $sponsors = Sponsor::all()->orderBy('rank', 'desc')->take(6);
+        $team_members = TeamMember::all()->orderBy('rank', 'desc')->get();
+        $professionals = Professional::all();
+        return view('about', compact("events", "coming_event", "sponsors", "team_members", "professionals"));
     }
 }
--- database/migrations/2024_08_30_124047_create_sponsors_table.php
+++ database/migrations/2024_08_30_124047_create_sponsors_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('url');
             $table->string('logo');
+            $table->integer('rank');
             $table->timestamps();
         });
     }
